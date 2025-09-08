b7a277df3da46c36f37c32cd1c40bea5bd31edb3
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2025-09-08 12:24:32 +0530

    user registration db part succesfully

--- src/main/java/com/musicstore/musicstore/controller/AuthController.java
+++ src/main/java/com/musicstore/musicstore/controller/AuthController.java
@@ -6,16 +6,20 @@ import com.musicstore.musicstore.service.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @AllArgsConstructor
+//@RequestMapping("/api/v1")
 public class AuthController {
 
     private final AuthService authService;
 
+    @PostMapping("/register")
     public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRq registerRq) {
 
         RegisterResponse registerResponse = authService.register(registerRq);
