269182e94403eed17ff6c6bb71f4e739f6bf9b89
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2025-09-12 17:08:40 +0530

    handle bad credentials exception class added to exception

--- src/main/java/com/musicstore/musicstore/exception/AppControllerAdviser.java
+++ src/main/java/com/musicstore/musicstore/exception/AppControllerAdviser.java
@@ -1,11 +1,17 @@
 package com.musicstore.musicstore.exception;
 
 import com.musicstore.musicstore.dto.response.error.CustomErrorResponse;
+import com.musicstore.musicstore.dto.response.error.ErrorResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class AppControllerAdviser {
@@ -20,5 +26,19 @@ public class AppControllerAdviser {
 
         return customErrorResponse;
 
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponseDto> handleBadCredentialsException(BadCredentialsException ex, HttpServletRequest request) {
+
+        ErrorResponseDto errorResponse = new ErrorResponseDto();
+
+        errorResponse.setTimestamp(LocalDateTime.now());
+        errorResponse.setStatus(HttpStatus.UNAUTHORIZED.value()); // 401
+        errorResponse.setError("Unauthorized");
+        errorResponse.setMessage("Invalid credentials. Please check your email and password."); // Custom message
+        errorResponse.setPath(request.getRequestURI());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 }
