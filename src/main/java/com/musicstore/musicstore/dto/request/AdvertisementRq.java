dea1f0b90bc56125fad7d25f05146464454ce24e
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2025-09-23 17:27:46 +0530

    advertistment rq created

---
+++ src/main/java/com/musicstore/musicstore/dto/request/AdvertisementRq.java
@@ -0,0 +1,13 @@
+package com.musicstore.musicstore.dto.request;
+
+import lombok.Data;
+
+@Data
+public class AdvertisementRq {
+
+
+    private String title;
+    private String description;
+    private String advertisementUrl;
+
+}
